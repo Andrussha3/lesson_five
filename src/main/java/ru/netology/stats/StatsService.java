2d0fa1c7610f70146c8c78e2ffcbeae99b7403a8
andy <lupolenkoandre@gmail.com>
2025-08-26 19:21:36 +0300

    initial commit

---
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -0,0 +1,76 @@
+package ru.netology.stats;
+
+import java.util.Arrays; //импортируем модуль, для подсчета суммы всех элементов в массиве
+
+public class StatsService {
+    // считаем сумму всех элементов
+    public int calculateTotalSales(long[] sales) {
+        long sum = Arrays.stream(sales).sum();
+        return (int) sum;
+    }
+
+    // считаем среднее значение среди всех элементов
+    public int calculateAverage(long[] sales) {
+        long sum = Arrays.stream(sales).sum();
+        long s = sum / sales.length;
+        return (int) s;
+    }
+
+    // находим номер месяца, в котором были максимальные продажи
+    public int maxSales(long[] sales) {
+        int maxMonth = 0;
+
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] > sales[maxMonth]) {
+                maxMonth = i;
+            }
+        }
+
+        return maxMonth + 1;
+    }
+
+    // считаем все месяца, где продажи были меньше среднего значения
+    public int countMonthsBelowAverage(long[] sales) {
+        //  Получаем среднее значение, используя ранее написанный метод svg
+        int average = calculateAverage(sales);
+        int count = 0;
+
+        // Проходим по всем месяцам и считаем те, где продажи ниже среднего
+        for (long sale : sales) {
+            if (sale < average) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    // считаем все месяца, где продажи были больше среднего значения
+    public int countMonthsAboveAverage(long[] sales) {
+        // 1. Получаем среднее значение, используя ранее написанный метод svg
+        int average = calculateAverage(sales);
+        int count = 0;
+
+        // Проходим по всем месяцам и считаем те, где продажи больше среднего
+        for (long sale : sales) {
+            if (sale > average) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    // находим номер месяца, в котором были минимальные продажи
+    public int minSales(long[] sales) {
+        int minMonth = 0;
+
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] < sales[minMonth]) {
+                minMonth = i;
+            }
+        }
+
+        return minMonth + 1;
+    }
+}
---
+++ src/test/java/ru/netology/stats/StatsServiceTests.java
@@ -0,0 +1,60 @@
+package ru.netology.stats;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class StatsServiceTests {
+
+    private StatsService service;       // ОБЪЯВЛЯЕМ поле для тестируемого сервиса
+    private long[] salesData;           // ОБЪЯВЛЯЕМ поле для тестовых данных
+
+    // Этот метод запускается ПЕРЕД КАЖДЫМ тестом
+    @BeforeEach
+    public void setUp() {   // Объявление метода настройки
+        service = new StatsService();// Создаем экземпляр (объект) класса StatsService для тестирования
+        salesData = new long[]{8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18}; // тестовые данные
+    }
+
+    @Test
+    public void testMaxSales() {
+        int expected = 6;
+        int actual = service.maxSales(salesData);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testMinSales() {
+        int expected = 9;
+        int actual = service.minSales(salesData);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testCalculateAverage() {
+        int expected = 15;
+        int actual = service.calculateAverage(salesData);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testCountMonthsBelowAverage() {
+        int expected = 5;
+        int actual = service.countMonthsBelowAverage(salesData);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testCountMonthsBeMoreAverage() {
+        int expected = 5;
+        int actual = service.countMonthsAboveAverage(salesData);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testCalculateTotalSales() {
+        long expected = 180;
+        long actual = service.calculateTotalSales(salesData);
+        Assertions.assertEquals(expected, actual);
+    }
+}
